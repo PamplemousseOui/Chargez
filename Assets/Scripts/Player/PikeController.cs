e2a606d497b68ef5e5ef147f3e32c9115710b960
Becher <mbelkhenchir@gmail.com>
2023-09-02 19:41:51 +0200

    Add arrow in front of player

---
+++ Assets/Scripts/Player/PikeController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PikeController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
