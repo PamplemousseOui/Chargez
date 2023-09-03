7fc3177ad0bead91f2c66a86837a6f84fdce26bb
Becher <mbelkhenchir@gmail.com>
2023-09-03 17:18:25 +0200

    wall starts farrer

--- Assets/Scripts/Managers/SpawnManager.cs
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -106,24 +106,24 @@ public class SpawnManager : MonoBehaviour
                         size = arenaSize.x;
                         spawnedEnemy.transform.localRotation = Quaternion.Euler(new Vector3(0,0,-180));
                         x = 0;
-                        y = arenaSize.y / 2 + 2;
+                        y = arenaSize.y / 2 + 5;
                         break;
                     case 1:
                         size = arenaSize.y;
                         spawnedEnemy.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 90));
-                        x = arenaSize.x / 2 + 2;
+                        x = arenaSize.x / 2 + 5;
                         y = 0;
                         break;
                     case 2:
                         size = arenaSize.x;
                         spawnedEnemy.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
                         x = 0;
-                        y = -arenaSize.y / 2 - 2;
+                        y = -arenaSize.y / 2 - 5;
                         break;
                     case 3:
                         size = arenaSize.y;
                         spawnedEnemy.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, -90));
-                        x = -arenaSize.x / 2 - 2;
+                        x = -arenaSize.x / 2 - 5;
                         y = 0;
                         break;
                 }
